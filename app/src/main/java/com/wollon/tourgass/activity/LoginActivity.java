163e993c4a09d82f780d13296c2eb36b0353c12a
e <cjhlearn@outlook.com>
2017-11-16 10:13:10 +0800

    更新项目结构与部分登录界面

---
+++ app/src/main/java/com/wollon/tourgass/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.wollon.tourgass.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 漫聆默 on 2017/11/16 0016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private LinearLayout llAccount;
+    private LinearLayout llPassword;
+    private EditText etAccount;
+    private EditText etPassword;
+    private Button btnLogin;
+    private CheckBox cbAutoLogin;
+
+
+
+
+}
--- app/src/main/java/com/wollon/tourgass/activity/MainActivity.java
+++ app/src/main/java/com/wollon/tourgass/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.wollon.tourgass;
+package com.wollon.tourgass.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wollon.tourgass.R;
 
 public class MainActivity extends AppCompatActivity {
 
