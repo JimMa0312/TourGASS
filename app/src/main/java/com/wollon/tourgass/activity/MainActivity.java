b767de226b1cd3f4ed2592569ad016a3ee15a2cd
JimMa <JimMa0312@outlook.com>
2017-11-17 08:29:11 +0800

    测试用户数据表的注册

--- app/src/main/java/com/wollon/tourgass/activity/MainActivity.java
+++ app/src/main/java/com/wollon/tourgass/activity/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.wollon.tourgass.R;
+import com.wollon.tourgass.dao.User;
+import com.wollon.tourgass.operator.impl.LoginImpl;
 
 public class MainActivity extends BaseActivity {
     private Button btAddData;
@@ -28,13 +31,26 @@ public class MainActivity extends BaseActivity {
         }
     };
 
+    private OnClickListener addDataListener=new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            User user=new User("admin","adnin","admin");
+            LoginImpl login=new LoginImpl();
+            if(login.register(user)){
+                Toast.makeText(context,"注册成功！",Toast.LENGTH_SHORT);
+            }else{
+                Toast.makeText(context,"注册失败！",Toast.LENGTH_SHORT);
+            }
+        }
+    };
+
     @Override
     protected void init() {
         setContentView(R.layout.activity_main);
 
         btAddData=(Button) findViewById(R.id.test_Add_bt);
         btLogin=(Button) findViewById(R.id.test_login);
-        btAddData.setOnClickListener(clickListener);
+        btAddData.setOnClickListener(addDataListener);
         btLogin.setOnClickListener(clickListener);
 
        /* Intent intent=new Intent();
